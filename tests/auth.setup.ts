7e587ba195fef562178ca5a05f6a8d6ad8c6c040
Missael <lmpadillar@gmail.com>
2026-07-02 04:50:28 -0700

    fix: streamline authentication state generation for smoke tests

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,48 +6,41 @@ import { createStorageState } from "./helpers/auth-state.helper";
 
 setup("Generate authentication states", async ({ page }) => {
   mkdirSync("playwright/.auth", { recursive: true });
+  const superAdminEmail =
+    process.env.STAGING_SUPERADMIN_EMAIL ?? process.env.QA_SUPERADMIN_EMAIL;
 
+  const superAdminPassword =
+    process.env.STAGING_SUPERADMIN_PASSWORD ??
+    process.env.QA_SUPERADMIN_PASSWORD;
+
+  if (!superAdminEmail || !superAdminPassword) {
+    throw new Error("Missing Super Admin credentials.");
+  }
+
+  const isStaging =
+    process.env.STAGING_SUPERADMIN_EMAIL &&
+    process.env.STAGING_SUPERADMIN_PASSWORD;
+    
   await createStorageState(
     page,
-    process.env.QA_SUPERADMIN_EMAIL!,
-    process.env.QA_SUPERADMIN_PASSWORD!,
+    superAdminEmail,
+    superAdminPassword,
     "playwright/.auth/superadmin.json",
   );
 
-  await createStorageState(
-    page,
-    process.env.QA_MANAGER_EMAIL!,
-    process.env.QA_MANAGER_PASSWORD!,
-    "playwright/.auth/manager.json",
-  );
+  if (!isStaging) {
+    await createStorageState(
+      page,
+      process.env.QA_MANAGER_EMAIL!,
+      process.env.QA_MANAGER_PASSWORD!,
+      "playwright/.auth/manager.json",
+    );
 
-  await createStorageState(
-    page,
-    process.env.QA_EMPLOYEE_EMAIL!,
-    process.env.QA_EMPLOYEE_PASSWORD!,
-    "playwright/.auth/employee.json",
-  );
+    await createStorageState(
+      page,
+      process.env.QA_EMPLOYEE_EMAIL!,
+      process.env.QA_EMPLOYEE_PASSWORD!,
+      "playwright/.auth/employee.json",
+    );
+  }
 });
-
-
-
-// setup("authenticate as super admin", async ({ page }) => {
-//   mkdirSync("playwright/.auth", { recursive: true });
-
-//   const response = await page.request.post("/api/auth/signin", {
-//     data: {
-//       email: process.env.QA_SUPERADMIN_EMAIL!,
-//       password: process.env.QA_SUPERADMIN_PASSWORD!,
-//     },
-//   });
-
-//   expect(response.status()).toBe(200);
-// //   expect(response.ok()).toBeTruthy();
-//   await page.goto("/dashboard");
-
-//   await expect(page).not.toHaveURL(/signin/);
-
-//   await page.context().storageState({
-//     path: "playwright/.auth/superadmin.json",
-//   });
-// });
